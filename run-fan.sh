2704d23e31bb37236bac83ab67f8beb09442fdc9
pandeyprakhar00 <41755577+pandeyprakhar00@users.noreply.github.com>
2020-03-28 00:02:55 +0530

    Create run-fan.sh

---
+++ run-fan.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          run-fan.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+# If you want a command to always run, put it here
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+start)
+echo "Starting run-fan.py"
+    /usr/local/bin/run-fan.py &
+    ;;
+stop)
+echo "Stopping run-fan.py"
+pkill -f /usr/local/bin/run-fan.py
+    ;;
+  *)
+echo "Usage: /etc/init.d/run-fan.sh {start|stop}"
+exit 1
+    ;;
+esac
+
+exit 0
