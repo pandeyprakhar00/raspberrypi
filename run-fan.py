5b6bdfe04192c7c4307c6ab1a9b58bfc8bf2699b
pandeyprakhar00 <41755577+pandeyprakhar00@users.noreply.github.com>
2020-03-30 18:19:01 +0530

    Update run-fan.py

--- run-fan.py
+++ run-fan.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-importos
+import os
 import time
-importRPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 pin = 21 # The pin ID, edit here to change it
@@ -12,24 +12,24 @@ minTMP = 35 # The low temperature in Celsius which will trigger fan off
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 GPIO.setwarnings(False)
-
-defgetCPUtemperature():
-res = os.popen('vcgencmdmeasure_temp').readline()
-temp =(res.replace("temp=","").replace("'C\n",""))
+    
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    temp =(res.replace("temp=","").replace("'C\n",""))
     #print(temp)
-return temp
+    return temp
 
-defgetTEMP():
-CPU_temp = float(getCPUtemperature())
-ifCPU_temp>maxTMP:
-GPIO.output(pin, True)
-elifCPU_temp<minTMP:
-GPIO.output(pin, False)
-return()
+def getTEMP():
+    CPU_temp = float(getCPUtemperature())
+    if CPU_temp>maxTMP:
+        GPIO.output(pin, True)
+    elif CPU_temp<minTMP:
+        GPIO.output(pin, False)
+    return()
 
 try:
-while True:
-getTEMP()
-time.sleep(5) # Read the temperature every 5 sec, increase or decrease this limit if you want
-exceptKeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
-GPIO.cleanup() # resets all GPIO ports used by this program
+    while True:
+        getTEMP()
+        time.sleep(5) # Read the temperature every 5 sec, increase or decrease this limit if you want
+except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
+    GPIO.cleanup() # resets all GPIO ports used by this program
